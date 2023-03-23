77d556436b6eba5bf2609fd4ea3ad4f1e076531d
John Loyer <johnloyer@ufl.edu>
2023-03-22 20:00:18 -0400

    i added my decoder function

--- lab06.py
+++ lab06.py
@@ -10,12 +10,34 @@ def encode_password(password):
         encoded_password += new_digit
     return encoded_password
 
+
 def decode_password(encoded_password):
     decoded_password = ''
     for digit in encoded_password:
         new_digit = str((int(digit) - 3) % 10)
         decoded_password += new_digit
     return decoded_password
+
+
+def decoder(user_password):
+    numbers = []
+    decoded_value = 0
+    for num in user_password:
+        string_to_num = int(num)
+        decoded_value = string_to_num - 3
+        if decoded_value == -1:
+            decoded_value = 9
+        elif decoded_value == -2:
+            decoded_value = 8
+        elif decoded_value == -3:
+            decoded_value = 7
+
+        numbers.append(str(decoded_value))
+
+    numbers = ''.join(numbers)
+
+    return numbers
+
 
 while True:
     print("Menu")
@@ -32,7 +54,7 @@ while True:
         print("Encoded password: " + encoded_password)
     elif choice == '2':
         encoded_password = input("Please enter your password to decode: ")
-        decoded_password = decode_password(encoded_password)
+        decoded_password = decoder(encoded_password)
         print("Your password has been decoded and stored!")
         print("Decoded password: " + decoded_password)
     elif choice == '3':
@@ -40,7 +62,5 @@ while True:
         break
     else:
         print("Invalid option. Please try again.")
-
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
